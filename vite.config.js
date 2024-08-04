805a57c14b8e0e11381910c1d77a5934464566c5
liudewa888 <2549711475@qq.com>
2024-08-04 17:05:20 +0800

    fix: resolve can not found

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 import vue from "@vitejs/plugin-vue";
 import tailwindcss from "tailwindcss";
 import autoprefixer from "autoprefixer";
